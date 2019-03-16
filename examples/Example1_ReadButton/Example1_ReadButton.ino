ced2e6613693843e86684f0556bdba2fc6b43527
Pete Lewis <pete.lewis@sparkfun.com>
2019-03-15 23:32:26 -0600

    example 1 updated - fifo bit command update and version read working

--- examples/Example1_ReadButton/Example1_ReadButton.ino
+++ examples/Example1_ReadButton/Example1_ReadButton.ino
@@ -9,48 +9,56 @@
   https://www.sparkfun.com/products/14641
 
   This example prints which button was pressed. Press * for space and # for new line.
-  
+
   Qwiic KeyPad records any button presses to a stack. If quered by the master KeyPad will
   respond with the oldest button pressed along with the time since it was pressed.
 */
 
 #include <Wire.h>
-
-byte keypadAddress = 75; //75 (0x4B) is default, 74 if jumper is closed
+#include "SparkFun_Qwiic_Keypad_Arduino_Library.h" //Click here to get the library: http://librarymanager/All#SparkFun_keypad
+KEYPAD keypad1; //Create instance of this object
 
 void setup(void)
 {
-  Wire.begin();
-  
   Serial.begin(9600);
-  Serial.println("Read Qwiic KeyPad Example");
+  Serial.println("Qwiic KeyPad Example");
+
+  if (keypad1.begin() == false)
+  {
+    Serial.println("Joystick does not appear to be connected. Please check wiring. Freezing...");
+    while (1);
+  }
+
   Serial.println("Press a button: * to do a space. # to go to next line.");
+  keypad1.updateFIFO();
+      Serial.print(" Version: ");
+    Serial.println(keypad1.getVersion());
+  while(1);
 }
 
 void loop(void)
 {
-  char button = readKeyPad();
-
-  if(button == -1)
-  {
-    Serial.println("No keypad detected");
-    delay(1000);
-  }
-  else if(button != 0)
-  {
-    if(button == '#') Serial.println();
-    else if(button == '*') Serial.print(" ");
-    else Serial.print(button);
-  }
+//  char button = readKeyPad();
+//
+//  if (button == -1)
+//  {
+//    Serial.println("No keypad detected");
+//    delay(1000);
+//  }
+//  else if (button != 0)
+//  {
+//    if (button == '#') Serial.println();
+//    else if (button == '*') Serial.print(" ");
+//    else Serial.print(button);
+//  }
 
   //Do something else. Don't call readKeyPad a ton otherwise you'll tie up the I2C bus
   delay(25); //25 is good, more is better
 }
 
-//Get the latest button
-char readKeyPad()
-{
-  Wire.requestFrom((uint8_t)keypadAddress, (uint8_t)1);
-  return(Wire.read());
-}
-
+////Get the latest button
+//char readKeyPad()
+//{
+//  Wire.requestFrom((uint8_t)keypadAddress, (uint8_t)1);
+//  return (Wire.read());
+//}
